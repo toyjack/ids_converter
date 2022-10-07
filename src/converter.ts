671c845faf03733b6bb79369414478ba066f9fb1
toyjack <toyjack@gmail.com>
2022-10-07 09:35:10 +0900

    cleaning

--- src/converter.ts
+++ src/converter.ts
@@ -58,10 +58,3 @@ export function convert(ids: string) {
   }
   return treeStack[0];
 }
-
-// const tester = '⿱A⿰⿵CD⿴⿵CDE';
-// const tester = '⿰⿱卯金刂';
-// const tester = '⿱A⿰B⿴⿵CD⿷EF';
-// const tester = '⿱⿱ABC';
-// const tester = '⿱⿱AB⿲DEF';
-// console.log(JSON.stringify(convert(tester), null, 2));
